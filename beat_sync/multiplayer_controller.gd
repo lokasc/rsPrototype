b0032c4d36bac9b1ec9448f6a5c5cb74178464cb
lokasc <sc21lphc@leeds.ac.uk>
2024-08-02 15:01:07 +0800

    Fixed local connection
    - You can now connect to your own machine

--- beat_sync/multiplayer_controller.gd
+++ beat_sync/multiplayer_controller.gd
@@ -7,36 +7,50 @@ const MAX_CLIENTS = 2
 @export var game_ui : Control
 
 
-var ip_address = "" # determined via user input.
+var ip_address = "127.0.0.1"
 var peer
 
-
 func _ready():
+	print(IP.get_local_addresses()[3])
+	# Initialise signals.
+	multiplayer.peer_connected.connect(_on_player_connect)
+	multiplayer.peer_disconnected.connect(_on_player_disconnect)
+	multiplayer.connection_failed.connect(_on_connection_failed)
+	multiplayer.connected_to_server.connect(_on_connected_to_server)
+	
 	net_ui.visible = true
 	game_ui.visible = false
 
 func _process(delta):
+	if peer != null:
+		print(peer.get_connection_status())
 	pass
 
 # Creates a client given ip_address
 func innit_client():
 	peer = ENetMultiplayerPeer.new()
-	peer.create_client(ip_address, PORT)
+	var error = peer.create_client(ip_address, PORT)
+	
+	if error != OK:
+		OS.alert("Shit input?")
+		peer = null
+		error = null
+		return
+	
 	multiplayer.multiplayer_peer = peer
+	
+	net_ui.visible = false
+	game_ui.visible = true
+	(game_ui.get_child(0) as Label).text = "Connecting to: " + ip_address
 
 # Creates a server
 func innit_host():
 	peer = ENetMultiplayerPeer.new()
 	peer.create_server(PORT, MAX_CLIENTS)
 	multiplayer.multiplayer_peer = peer
-
-func join_server():
-	# joins server.
-	pass
-
+	print("Server started on port " + str(PORT) + " with a maximum of " + str(MAX_CLIENTS) + " clients")
 
 func _on_host_button_button_down():
-	print("HOST PRESSED!")
 	innit_host()
 	
 	# set initial ui
@@ -49,18 +63,28 @@ func _on_host_button_button_down():
 func _on_join_button_button_down():
 	print("CLIENT PRESSED!")
 	
-	var ip_node = get_node("/root/BeatMain/UI/NetUI/HBoxContainer/IP") as TextEdit
-	ip_address = ip_node.text
+	var input_ip = (get_node("/root/BeatMain/UI/NetUI/HBoxContainer/IP") as TextEdit).text
+	if input_ip != "":
+		ip_address = input_ip
 	
 	# check if null
 	if (ip_address == "" or ip_address == null):
 		OS.alert("Need an IP to connect to")
 		return
-
-	# check for timeout on connection.
-
+		
 	innit_client()
-	net_ui.visible = false
-	game_ui.visible = true
-	
+
+# TOOD: Want to set up showing whos on connected.
+func _on_player_connect(id):
+	print(id)
+	pass
+
+func _on_player_disconnect(id):
+	pass
+
+func _on_connection_failed():
+	pass
+
+func _on_connected_to_server():
 	(game_ui.get_child(0) as Label).text = "ID: " + str(multiplayer.get_unique_id()) + " (Client)"
+	pass
