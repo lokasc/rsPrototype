a648c2c84c254bee7fcabe08cb2e3848a5b74c71
SpicyMango301 <godric301@gmail.com>
2024-08-02 09:37:12 +0100

    Player hitbox debug now changes color
    - changed HAI to attack for clearer prints
    - attack hitbox changes colour so its clearer when it is attacking

--- player_controller.gd
+++ player_controller.gd
@@ -3,12 +3,12 @@ extends CharacterBody2D
 ## This script controls the player's movement and handles any other inputs
 ## with regards to the player
 
-
 const SPEED = 400
 const DECELERATION = 80
 
 @export var health = 100
 @onready var attack_box = $AttackBox
+@onready var collision_box = $AttackBox/CollisionShape2D
 
 var stats_dictionary = { # placed in a dictionary for easy understanding, will be in a class later.
 		"spd" : 10,
@@ -23,7 +23,7 @@ func _ready():
 
 func _process(delta):
 	if Input.is_action_just_pressed("attack"):
-		print("HAI!")
+		print("Attack!")
 		basic_attack()
 
 func _physics_process(delta):
@@ -45,8 +45,10 @@ func basic_attack():
 	# start animation of attack.
 	# for now, activate a hitbox in front of the player
 	attack_box.monitoring = true
+	collision_box.debug_color = Color("dd488d6b")
 	await get_tree().create_timer(2).timeout
 	attack_box.monitoring = false
+	collision_box.debug_color = Color("0099b36b")
 
 func _on_attack_box_area_entered(area):
 	# enemys have an hurtbox area2D node.
