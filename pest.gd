84a0ba2a46cd08ceca694c5a623a303a6fb6d59c
SpicyMango301 <godric301@gmail.com>
2024-08-02 09:34:56 +0100

    Code Refactoring in pest
    Combines global_position.x and .y
    
    distance is calculated using distance_to function

--- pest.gd
+++ pest.gd
@@ -10,15 +10,16 @@ func _init():
 func _physics_process(delta):
 	# direction the pest needs to go towards:
 	var direction = target.global_position - global_position
-	var distance = sqrt(
-			pow(target.global_position.x - global_position.x,2) + 
-			pow(target.global_position.y - global_position.y,2)
-	)
+	var distance = global_position.distance_to(target.global_position)
 	
-	print(distance)
-	#global_position.distance_to(target.global_position)
+	#sqrt(
+			#pow(target.global_position.x - global_position.x,2) + 
+			#pow(target.global_position.y - global_position.y,2)
+	#)
 	
-	if distance < 100:
+	#print(distance)
+	
+	if distance < 15:
 		velocity = Vector2.ZERO
 	else:
 		velocity = direction * speed
@@ -30,5 +31,5 @@ func _decide(target = null):
 		return
 	
 	# move towards player
-	global_position.x = move_toward(global_position.x, target.global_position.x, speed)
-	global_position.y = move_toward(global_position.y, target.global_position.y, speed)
+	global_position = global_position.move_toward(target.global_position, speed)
+
