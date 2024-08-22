6b897a23030fa30fecba9619108cc172a9eb0d26
SpicyMango301 <godric301@gmail.com>
2024-08-22 17:23:02 +0100

    Selection Card Rework
    - Changed theme so the font is consistent
    - The button now extends to cover the entire card
    - changed button theme so it is easier to see

--- ver_1/game/UI/selection_card.gd
+++ ver_1/game/UI/selection_card.gd
@@ -1,21 +1,23 @@
 class_name SelectionCard
-extends Button
+extends Control
 
 signal client_card_selected(selected_card)
 var action : BaseAction
 var hero : BaseHero
 
+@onready var vbox : VBoxContainer = $VBoxContainer
 @onready var name_label = $VBoxContainer/Name as RichTextLabel
 @onready var icon_texture = $VBoxContainer/TextureRect as TextureRect
 @onready var status = $VBoxContainer/Status as RichTextLabel
 @onready var desc = $VBoxContainer/Description as RichTextLabel
-
+@onready var button : Button = $Button
 
 func _ready() -> void:
 	self.client_card_selected.connect(GameManager.Instance.tell_server_client_is_ready)
 	name_label.text = "[center]" + action.get_class_name() + "[/center]" 
 	hero = GameManager.Instance.local_player
-	desc.text = action.desc
+	desc.text = "[center]" + action.desc + "[/center]" 
+	button.size = vbox.size
 	
 	status.text = get_status_string()
 
