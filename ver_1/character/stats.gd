f090efd5c1b0a0220af39cf3cdf560842e8740bf
SpicyMango301 <godric301@gmail.com>
2024-08-22 08:55:41 +0100

    changed atk_mul stat from int to float

--- ver_1/character/stats.gd
+++ ver_1/character/stats.gd
@@ -4,17 +4,17 @@ class_name Stats
 # unless they have a special effect.
 
 
-@export var maxhp : int # Max Health
-@export var aoe : int	# Area of Effect
-@export var arm : int	# Armour
-@export var atk : int	# Attack Damage
-@export var spd : int	# Movement Speed
-@export var cd : int	# Cooldown nb: atk speed
-@export var pick : int	# Pick up radius
-@export var mus : int	# Music multiplier (benefits from music sync)
-@export var hsg : float	# Heal & Shield gain
-@export var dur : int	# Duration of abilities
-@export var atk_mul : int	# Attack Multiplier
+@export var maxhp : int 	# Max Health
+@export var aoe : int		# Area of Effect
+@export var arm : int		# Armour
+@export var atk : int		# Attack Damage
+@export var spd : int		# Movement Speed
+@export var cd : int		# Cooldown nb: atk speed
+@export var pick : int		# Pick up radius
+@export var mus : int		# Music multiplier (benefits from music sync)
+@export var hsg : float		# Heal & Shield gain
+@export var dur : int		# Duration of abilities
+@export var atk_mul : float # Attack Multiplier
 
 
 func _init(_maxhp = 100, _aoe = 1, _arm = 1, _atk = 1, _spd = 1, _cd = 1, _pick = 1, _mus = 1, _hsg = 1, _dur = 1, _atk_mul = 1):
