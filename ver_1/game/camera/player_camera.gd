2a2a3ef49663eaeec4b3d5a84501a3a9dc834ea3
SpicyMango301 <godric301@gmail.com>
2024-08-26 16:00:19 +0100

    Enemies outside of screen will be invisible
    - Apparently this helps with processing and will be twice as fast?
    - it seems faster when they are off-screen

--- ver_1/game/camera/player_camera.gd
+++ ver_1/game/camera/player_camera.gd
@@ -5,7 +5,18 @@ extends Camera2D
 ## Currently, it just follows the player.
 
 @export var target : NodePath #Assign the node this camera will follow.
+@onready var visibility_notifier = $Area2D/CollisionShape2D
 
 func _physics_process(_delta:float):
+	visibility_notifier.shape.size = get_viewport().get_visible_rect().size
 	if target:
-		global_position = get_node(target).global_position
+		global_position = get_node(target).global_position 
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.get_parent() is BaseEnemy:
+		area.get_parent().show()
+
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	if area.get_parent() is BaseEnemy:
+		area.get_parent().hide()
