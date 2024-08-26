0389f5cb9ef3f3819f44a6b09d6ecd5fb6283f7f
lokasc <sc21lphc@leeds.ac.uk>
2024-08-26 17:28:26 +0800

    Added Interactive Music files & resources
    - Changed is on beat function to accommodate for grace period

--- ver_1/game/music/beat_controller.gd
+++ ver_1/game/music/beat_controller.gd
@@ -3,19 +3,24 @@ extends Node
 
 signal on_beat # signal for executing things on to the beat. 
 
+@onready var main_music_player = $MainMusicPlayer
+@onready var hihat_sound = $AudioStreamPlayer
+@onready var test_interactive  : AudioStreamInteractive = load("res://Resources/test_interactive.tres")
+
+@export var grace_time : float # +- time for checks
+
 var beat_timer : Timer
 var current_beat_time : float # time until beat is hit. 
 
-@onready var hihat_sound = $AudioStreamPlayer
-@onready var main_music_player = $MainMusicPlayer
-@onready var test_interactive  : AudioStreamInteractive = load("res://Resources/test_interactive.tres")
-
+var is_playing : bool # is music track playing?
 var beat_duration : float # how many seconds a beat is (or till the next beat)
 var current_music : AudioStreamInteractive
-var is_playing : bool
-var grace_time : float # +- time for checks
-
 var counter : int = 0
+
+# Latency
+var time_begin
+var time_delay
+var previous_time = 0
 
 
 func _enter_tree() -> void:
@@ -31,63 +36,56 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("attack"):
 		change_clip()
 	
-	
 	if !is_playing: return
+	
+	# Compensate for delay.
+	var time = (Time.get_ticks_usec() - time_begin) / 1000000.0
+	# Compensate for latency.
+	time -= time_delay
+	# May be below 0 (did not begin yet).
+	time = max(0, time)
+	
+	#current_beat_time += time - previous_time
+	#if current_beat_time >= beat_duration:
+		#current_beat_time = 0
+		#hihat_sound.stop()
+		#hihat_sound.play()
+	
+	previous_time = time
 	current_beat_time += delta
 	
 	if current_beat_time >= beat_duration:
 		current_beat_time = 0
-		#hihat_sound.stop()
-		#hihat_sound.play()
+		#(main_music_player.stream as AudioStreamSynchronized).get_sync_stream(1).play()
 		on_beat.emit()
 
-# TODO: change arguements to system time & BC script variables
-func is_on_beat(BPM : int, current_time : float, grace_time : float) -> bool:
-	var second_one_beat : float = 60.0/BPM
-	if abs(current_time - second_one_beat) <= grace_time:
+# use this function to check when a input is on beat.
+func is_on_beat() -> bool:
+	if current_beat_time <= grace_time || current_beat_time >= beat_duration - grace_time:
 		return true
 	else:
 		return false 
 
+#region Internal
 func start_music() -> void:
 	is_playing = true
 	current_beat_time = beat_duration
+	
+	# Get latency.
+	time_begin = Time.get_ticks_usec()
+	time_delay = AudioServer.get_time_to_next_mix() + AudioServer.get_output_latency()
+	
 	main_music_player.play()
 	#beat_timer.start()
 
 func change_clip():
 	counter += 1
-	(main_music_player.get_stream_playback() as AudioStreamPlaybackInteractive).switch_to_clip(counter%2)
+	(main_music_player.get_stream_playback().switch_to_clip(counter%2))
 
-func change_music(new_music : AudioStream) -> void:
+func change_music(new_music : AudioStreamInteractive) -> void:
 	current_music = new_music
 	main_music_player.stream = current_music
-	beat_duration = 60.0/120
-	
-	# Godot's StreamInteractive is not flexible enough,
-	# cant transition from this bar to the next bar.
-	# Transition from calm to fast quicker.
-	current_music.add_transition(
-		0,  # index 0 is calm 
-		1,  # index 1 is fast
-		AudioStreamInteractive.TRANSITION_FROM_TIME_IMMEDIATE,
-		AudioStreamInteractive.TRANSITION_TO_TIME_SAME_POSITION,
-		AudioStreamInteractive.FADE_AUTOMATIC,
-		0.5
-		 )
-	
-	# Transition from fast to calm slower
-	current_music.add_transition(
-		1, 
-		0, 
-		AudioStreamInteractive.TRANSITION_FROM_TIME_IMMEDIATE,
-		AudioStreamInteractive.TRANSITION_TO_TIME_SAME_POSITION,
-		AudioStreamInteractive.FADE_AUTOMATIC,
-		1
-		 )
-
-func transition_music(from_clip, to_clip):
-	pass
+	beat_duration = 60/120
 
 @rpc("authority", "call_remote", "reliable")
 func stc_check_timestamp(time : float) -> void:
@@ -104,14 +102,18 @@ func cts_return_time_stamp(client_time : float) -> void:
 func sync_music() -> void:
 	pass
 
-func get_current_timestamp() -> float:
-	return 0
-
+#endregion
 ### Features we need
 # 1. a timer that sends a signal every beat [Done]
 # 2. a variable for grace period [Done]
-# 3. a boolean to check is something is on beat [in progress]
-# 4: interactive music, switching to pieces of same bar/timestamp. 
-# 5:  a function for setting fades between different music pieces
+# 3. a boolean to check is something is on beat [Done]
+# 4: interactive music, switching to pieces of same bar/timestamp. [In progress]
+# 5:  a function for setting fades between different music pieces [In progress]
 # 6. ensure music timestamps are similar, if not redirect.
 # 7. the check for on-beat is client-side or has leniancy for the client.
+
+
+func transition_music(from_clip, to_clip):
+	pass
+func get_current_timestamp() -> float:
+	return 0
