77c33779e692c733291ec9250171e35e2de01b7f
Lucas Chan <56242638+lokasc@users.noreply.github.com>
2024-08-26 18:37:02 +0800

    Update minor_bug.gd

--- ver_1/character/enemy/MinorBug/minor_bug.gd
+++ ver_1/character/enemy/MinorBug/minor_bug.gd
@@ -49,20 +49,3 @@ func _decide(target = null):
 	if target == null:
 		return
 
-func move_to_target(target = null):
-# direction the pest needs to go towards:
-	var direction = global_position.direction_to(target.global_position)
-	var distance = global_position.distance_to(target.global_position)
-	
-	if distance < 15:
-		velocity = Vector2.ZERO
-	else:
-		velocity = direction * char_stats.spd
-	
-	if direction.x < 0:
-		sprite.scale.x = x_scale
-	elif direction.x > 0:
-		sprite.scale.x = x_scale * -1
-	else:
-		sprite.scale.x = x_scale * -1
-	move_and_slide()
