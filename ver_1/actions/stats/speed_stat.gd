e28d34aa30bc8d4e7d581030afb114e5ffc933db
SpicyMango301 <godric301@gmail.com>
2024-09-03 11:24:37 +0100

    reverted maxhp and speed stat card mix up
    somehow the scripts got flipped

--- ver_1/actions/stats/speed_stat.gd
+++ ver_1/actions/stats/speed_stat.gd
@@ -1,12 +1,12 @@
-class_name MaxHPCard
+class_name SpeedCard
 extends BaseStatCard
 
 func _init() -> void:
 	action_icon_path = "res://assets/icons/heart-plus.png"
-	amount_per_upgrade = 50
+	amount_per_upgrade = 5
 
 func _upgrade():
 	super()
 	if level >= 5: return # do not increase health after level 5
-	hero.char_stats.maxhp += int(amount_per_upgrade)
-	#print("I upgraded my hp to %i", hero.char_stats.spd)
+	hero.char_stats.spd += int(amount_per_upgrade)
+	#print("I upgraded my hp to %i", hero.char_stats.maxhp)
