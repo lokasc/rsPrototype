6af831f06a3acf819dc1d74587f2f3e03cdbb685
SpicyMango301 <godric301@gmail.com>
2024-09-05 11:36:55 +0100

    knockback fix
    it doesn't pass objects through, it now passes the player position through instead

--- ver_1/status_effects/knockback.gd
+++ ver_1/status_effects/knockback.gd
@@ -5,16 +5,16 @@ const SPEED = 1
 
 var kb_distance : float # the distance at which the character is knocked back
 var kb_speed_multiplier : float # multiplier of speed of the knockback finishing, default is 1
-var player : BaseHero # the hero that applied the knockback
+var player_pos : Vector2 # the position of the hero that applied the knockback
 
 var original_pos : Vector2
 var new_pos : Vector2
 var direction : Vector2
 
 # Constructor, affects new() function for creating new copies.
-func _init(_kb_distance : float, _player : BaseHero, _kb_speed_multiplier : float = 1):
+func _init(_kb_distance : float, _player_pos : Vector2, _kb_speed_multiplier : float = 1):
 	kb_distance = _kb_distance
-	player = _player
+	player_pos = _player_pos
 	kb_speed_multiplier = _kb_speed_multiplier
 	pass
 
@@ -22,7 +22,7 @@ func on_added():
 	if character is BaseHero:
 		character.input.canMove = false
 	original_pos = character.position
-	direction = player.position.direction_to(original_pos)
+	direction = player_pos.direction_to(original_pos)
 	new_pos = original_pos + direction * kb_distance
 
 func update(_delta:float):
