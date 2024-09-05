6d9c0a33a4640715d1ad6c245e4f37a05a07cd5e
SpicyMango301 <godric301@gmail.com>
2024-09-05 15:34:42 +0100

    Added temporary effects for trebbie buff
    - this is done using particles with an aesprite particle texture

--- ver_1/character/hero/Trebbie/trebbie_buff.gd
+++ ver_1/character/hero/Trebbie/trebbie_buff.gd
@@ -32,6 +32,7 @@ var duration_time : float
 @onready var hitbox_shape : CollisionShape2D = $HitBox/CollisionShape2D
 @onready var hitbox : Area2D = $HitBox
 @onready var recast_timer : Timer = $BuffRecastTimer
+@onready var buff_particles : GPUParticles2D = $"../Sprites/BuffParticles2D"
 
 
 # Initialize abilities
@@ -50,6 +51,9 @@ func _ready() -> void:
 	hitbox.area_entered.connect(on_hit)
 	hitbox.visible = false
 	hitbox.monitoring = false
+	buff_particles.emitting = false
+	
+	initial_effect_scale = Vector2.ONE
 	if zero_cd:
 		a_stats.cd = 0
 	hitbox_shape.shape.radius = a_stats.aoe
@@ -57,6 +61,9 @@ func _ready() -> void:
 func enter() -> void:
 	hitbox.visible = true
 	hitbox.monitoring = true
+	buff_particles.emitting = true
+	buff_particles.process_material.scale = initial_effect_scale
+	buff_particles.show()
 	duration_time = 0 
 	recast = 0
 	set_ability_to_hero_stats()
@@ -69,6 +76,8 @@ func exit() -> void:
 	start_cd()
 	hitbox.visible = false
 	hitbox.monitoring = false
+	buff_particles.emitting = false
+	buff_particles.hide()
 	is_synced = false
 
 func update(delta: float) -> void:
@@ -107,33 +116,32 @@ func on_hit(area : Area2D) -> void:
 func _upgrade() -> void:
 	super()
 
-# Called automatically when ability cd finishes, override this to addd functionality when cd finishes
-func _on_cd_finish() -> void:
-	_reset()
-
-# Resets ability, lets players to use it again, override this to add functionality.
-func _reset() -> void:
-	super()
-
 func start_recast_logic() -> void:
 		hitbox.visible = false
 		hitbox.monitoring = false
+		buff_particles.hide()
 		# Starts checking if player will press within recast window
 		if recast_timer.is_stopped():
 			recast_timer.start(recast_window)
 		# Have to recast on beat
 		if hero.input.ability_1 and hero.input.is_on_beat and recast < recast_amount:	# Activated twice (reactivated)
-			hitbox_shape.shape.radius *= beat_sync_multiplier * hero.char_stats.mus
-			hitbox.visible = true
-			hitbox.monitoring = true
-			duration_time = 0
-			recast_timer.start(recast_window)
-			recast += 1
+			recast_ability()
 		# Resets if recasted too many times or didn't press on beat
 		elif hero.input.ability_1:
 			if recast >= recast_amount or hero.input.is_on_beat == false:
+				buff_particles.emitting = false
 				recast_timer.timeout.emit()
 				state_change.emit(self, "TrebbieAttack")
+
+func recast_ability():
+	hitbox_shape.shape.radius *= beat_sync_multiplier * hero.char_stats.mus
+	hitbox.visible = true
+	hitbox.monitoring = true
+	buff_particles.show()
+	buff_particles.process_material.scale *= beat_sync_multiplier * hero.char_stats.mus
+	duration_time = 0
+	recast_timer.start(recast_window)
+	recast += 1
 
 func _on_buff_recast_timer_timeout() -> void:
 	if recast <= recast_amount:
