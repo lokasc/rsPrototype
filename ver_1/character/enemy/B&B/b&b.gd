990750a6be1e48b2235cd01f2754f4649ca71bbe
SpicyMango301 <godric301@gmail.com>
2024-09-06 11:41:33 +0100

    Added B&B Scene and enemy name changes
    - Changed Char Names of enemies
    - Added B&B and each separately
    - Added Piano key sprites, contains different piano keys for animation
    - Added Hit Circle sprites, contains different red circles for animation

---
+++ ver_1/character/enemy/B&B/b&b.gd
@@ -0,0 +1,33 @@
+class_name BeethovenAndBiano
+extends BaseEnemy
+
+@onready var hitbox : Area2D = $HitBox
+@onready var collidebox : CollisionShape2D = $CollisionBox
+
+@export var update_frequency : float = 3
+var current_update_time : float = 0
+
+func _init() -> void:
+	super()
+	max_health = 1000
+	
+func _enter_tree() -> void:
+	super()
+	# assign random time, so enemies dont update all together
+	if multiplayer.is_server():
+		current_update_time = randf_range(1, update_frequency)
+	pass
+
+func _ready() -> void:
+	super()
+
+
+func _process(delta: float) -> void:
+	pass
+
+func _physics_process(_delta:float) -> void:
+	pass
+
+func on_hit(area : Area2D) -> void:
+	if !multiplayer.is_server(): return
+	
