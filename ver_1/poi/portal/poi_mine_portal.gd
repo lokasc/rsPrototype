f14b853855172275b11bae00a57594b7092545a8
lokasc <sc21lphc@leeds.ac.uk>
2024-09-16 18:06:27 +0800

    Added teleportation functionality for portals

--- ver_1/poi/portal/poi_mine_portal.gd
+++ ver_1/poi/portal/poi_mine_portal.gd
@@ -9,7 +9,10 @@ signal portal_unlocked
 @export var unlocked : bool
 @export var started : bool = false
 
-@export_category("POI Stats")
+@export_category("POI Information")
+@export var teleport_end_location : Vector2 = Vector2.ZERO
+
+@export_subgroup("POI Stats")
 # progress is measured in seconds
 @export var unlock_amount : float
 
@@ -28,20 +31,41 @@ func mining_logic(delta : float):
 	if current_progress >= unlock_amount:
 		# Unlock the portal logic.
 		if !multiplayer.is_server(): return
-		stc_set_unlocked.rpc()
+		stc_portal_unlocked.rpc()
 	current_progress += delta * unlock_speed
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_start_trigger_area_entered(area: Area2D) -> void:
 	if !multiplayer.is_server(): return
 	 
-	if body.get_parent() is not BaseHero: return
+	if started: return
+	if area.get_parent() is not BaseHero: return
 	stc_start_mining.rpc()
+
+func _on_teleport_trigger_area_entered(area: Area2D) -> void:
+	if !started || !unlocked: return 
+	if !multiplayer.is_server(): return
+	
+	
+	print("Emit")
+	# teleport player
+	var hero = area.get_parent()
+	if hero is not BaseHero || !hero: return
+	
+	hero.global_position = teleport_end_location
+	
+	
+	pass # Replace with function body.
 
 @rpc("reliable", "call_local")
 func stc_start_mining():
 	started = true
+	$BeatSyncEffect.modulate = Color.WHITE
+	$BeatSyncEffect.restart()
 
 @rpc("reliable", "call_local")
-func stc_set_unlocked():
+func stc_portal_unlocked():
 	unlocked = true
 	portal_unlocked.emit()
+	$BeatSyncEffect.scale = Vector2(10,10)
+	$BeatSyncEffect.modulate = Color.CADET_BLUE
+	$BeatSyncEffect.restart()
