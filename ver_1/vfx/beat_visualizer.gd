f2393953e2a47f1b2b443789e66060dc4ef8f606
SpicyMango301 <godric301@gmail.com>
2024-09-23 11:38:10 +0100

    Added more functionality to beat visualizer with lines
    - has text that says bad, good, great or perfect
    - multiple customizable variables
    - changed player ui to include rings and lines
    - MAKE SURE THAT TEXT POP UP IS IN THE SCENE WHERE IS_LINES IS TRUE

--- ver_1/vfx/beat_visualizer.gd
+++ ver_1/vfx/beat_visualizer.gd
@@ -1,6 +1,7 @@
 extends Control
 
-@export var is_lines : bool
+signal ability_input_pressed
+
 @export var max_size : float
 @export var min_size : float
 @export var time_to_max_size : float
@@ -20,12 +21,24 @@ extends Control
 ## the color of the outer trans ring when IT IS on beat
 @export var beating_color : Color
 
+@export_category("Lines")
+@export var is_lines : bool
+@export var textpopup_pos_offset : Vector2
+@export var great_grace_time : float
+@export var perfect_grace_time : float
+@export var perfect_color : Color
+@export var great_color : Color
+@export var good_color : Color
+@export var bad_color : Color
+
+var text_color : Color
 var opaque_ring : Resource = preload("res://ver_1/vfx/opaque_ring.tscn")
 var opaque_lines : Resource = preload("res://ver_1/vfx/opaque_ring_lines.tscn")
 
 @onready var trans_ring : TextureRect = $TransparentRing
 @onready var particles : GPUParticles2D = $GPUParticles2D
 @onready var effect_timer : Timer = $EffectTimer
+@onready var text_pop_up : TextPopUp = $TextPopUp
 
 @onready var bc : BeatController = GameManager.Instance.bc
 var time_til_next_beat: float
@@ -35,7 +48,9 @@ func _ready() -> void:
 	if not is_lines:
 		$TransparentRing.scale = max_size * Vector2.ONE
 	#The beat does effects which give the illusion that the rings are arriving on time
-	bc.on_beat.connect(end_opaque_ring)
+		bc.on_beat.connect(end_opaque_ring)
+	if is_lines:
+		ability_input_pressed.connect(end_opaque_ring)
 	bc.on_beat.connect(spawn_opaque_ring)
 	
 	particles.process_material.color = particle_color
@@ -43,6 +58,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if is_lines:
+		if GameManager.Instance.local_player != null:
+			if GameManager.Instance.local_player.input.ability_1 or GameManager.Instance.local_player.input.ability_2:
+				ability_input_pressed.emit()
+				text_pop_up.set_pop(check_accuracy(bc.get_time_til_next_beat()), textpopup_pos_offset, text_color, 0.5)
 	pass
 
 func init_ring(ring : BeatTexture) -> void:
@@ -69,3 +89,17 @@ func end_opaque_ring() -> void:
 
 func _on_effect_timer_timeout() -> void:
 	trans_ring.self_modulate = transparent_color
+
+func check_accuracy(time_til_beat : float) -> String:
+	if time_til_beat <= perfect_grace_time|| time_til_beat >= 0.5 - perfect_grace_time:
+		text_color = perfect_color
+		return "Perfect"
+	elif time_til_beat <= great_grace_time|| time_til_beat >= 0.5 - great_grace_time:
+		text_color = great_color
+		return "Great"
+	elif time_til_beat <= bc.grace_time|| time_til_beat >= 0.5 - bc.grace_time:
+		text_color = good_color
+		return "Good"
+	else:
+		text_color = bad_color
+		return "Bad"
