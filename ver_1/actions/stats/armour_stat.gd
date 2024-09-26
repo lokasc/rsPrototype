3e39f649a07bec994bacf463ad0014eccac43d91
SpicyMango301 <godric301@gmail.com>
2024-09-26 10:49:16 +0100

    Crypto Mine now gives stat bonuses when complete
    - also edited how armour shows to the players

--- ver_1/actions/stats/armour_stat.gd
+++ ver_1/actions/stats/armour_stat.gd
@@ -5,7 +5,7 @@ func _init() -> void:
 	super()
 	action_icon_path = "res://assets/icons/armor_icon.png"
 	amount_per_upgrade = 0.3
-	display_upgrade_amount = "-"+ str(snapped(100-(100/(1+amount_per_upgrade)),1)) +"%"
+	display_upgrade_amount = "+"+ str((1+amount_per_upgrade)*10)
 	card_desc = "Reduces damage taken by a percentage"
 	
 	action_name = "Armour"
@@ -15,4 +15,4 @@ func _upgrade():
 	if level >= 5: return # do not increase health after level 5
 	hero.char_stats.arm += amount_per_upgrade
 	#print("I upgraded my hp to %i", hero.char_stats.maxhp)
-	desc = "Armour\n" + "-"+ str(snapped(100-(100/(1+level*amount_per_upgrade)),1)) +"%"
+	desc = "Armour\n" + str((1+amount_per_upgrade)*10*level)
--- ver_1/poi/portal/poi_mine_portal.gd
+++ ver_1/poi/portal/poi_mine_portal.gd
@@ -8,29 +8,52 @@ signal portal_unlocked
 @export_category("Game flags")
 @export var unlocked : bool
 @export var started : bool = false
-
 @export var ready_to_teleport : bool
+@export var ready_to_give_stats : bool
 
-@export_category("POI Information")
-@export var teleport_locations : Array[Node2D]
-
-@export_subgroup("POI Stats")
+@export_category("POI Info")
 # progress is measured in seconds
 @export var unlock_amount : float
 
 # rate of unlocking
 @export var unlock_speed : float = 1
 
+@export_group("POI Modes")
+@export var teleportation : bool = false
+@export var give_stats_bonuses: bool = true
+
+@export_subgroup("Teleport Info")
 # time to recharge
 @export var recharge_time : float = 5
+
+@export var teleport_locations : Array[Node2D]
+
+@export_subgroup("Stats Bonus Info")
+@export var aoe_increase : float 
+@export var armour_increase : float 
+@export var cooldown_decrease : float 
+@export var damage_increase : float 
+@export var duration_increase : float
+@export var heal_and_shield_increase : float 
+@export var max_hp_increase : float 
+@export var music_multiplier_increase : float 
+@export var pick_up_radius_increase : float 
+@export var speed_increase : float 
+
+@export_category("Debug")
+@export var fast_unlock : bool
 
 var current_progress : float = 0
 var teleport_charges : int
 var current_recharge_time : float
+var bonus_given : bool = false
 
 @onready var progress_bar : ProgressBar = $ProgressBar
+@onready var vfx : GPUParticles2D = $BeatSyncEffect
 
 func _ready() -> void:
+	if fast_unlock:
+		unlock_amount = 5
 	progress_bar.max_value = unlock_amount
 
 func _process(delta: float) -> void:
@@ -38,10 +61,12 @@ func _process(delta: float) -> void:
 	
 	if !unlocked:
 		mining_logic(delta)
-	else:
+	elif teleportation:
 		#print(ready_to_teleport)
 		#print(teleport_charges)
 		recharge_logic(delta)
+	elif give_stats_bonuses and !bonus_given:
+		stat_bonus_logic()
 
 # Cool down on portal is dealt with on the server (for sync hassle sake)
 func recharge_logic(delta : float):
@@ -59,6 +84,30 @@ func recharge_logic(delta : float):
 	current_recharge_time += delta
 	#progress_bar.value = current_recharge_time
 
+func stat_bonus_logic():
+	if !multiplayer.is_server(): return
+	
+	if !ready_to_give_stats: return
+	
+	if GameManager.Instance.net.MAX_CLIENTS == 1:
+		increase_stats(GameManager.Instance.players[0])
+	else:
+		increase_stats(GameManager.Instance.players[0])
+		increase_stats(GameManager.Instance.players[1])
+	bonus_given = true
+
+func increase_stats(player : BaseHero):
+	var player_stats = player.char_stats
+	player_stats.aoe += aoe_increase
+	player_stats.arm += armour_increase
+	player_stats.cd -= cooldown_decrease
+	player_stats.atk += damage_increase
+	player_stats.dur += duration_increase
+	player_stats.hsg += heal_and_shield_increase
+	player_stats.maxhp += max_hp_increase
+	player_stats.mus += music_multiplier_increase
+	player_stats.pick += pick_up_radius_increase
+	player_stats.spd += speed_increase
 
 func mining_logic(delta : float):
 	if current_progress >= unlock_amount:
@@ -111,22 +160,23 @@ func decide_teleport_location() -> Vector2:
 @rpc("reliable", "call_local")
 func stc_stop_mining():
 	started = false
-	$BeatSyncEffect.modulate = Color.WHITE
-	$BeatSyncEffect.emitting = false
+	vfx.modulate = Color.WHITE
+	vfx.emitting = false
 
 
 @rpc("reliable", "call_local")
 func stc_start_mining():
 	started = true
-	$BeatSyncEffect.modulate = Color.WHITE
-	$BeatSyncEffect.emitting = true
+	vfx.modulate = Color.WHITE
+	vfx.emitting = true
 
 @rpc("reliable", "call_local")
 func stc_portal_unlocked():
 	unlocked = true
 	teleport_charges = 2
 	ready_to_teleport = true
+	ready_to_give_stats = true
 	portal_unlocked.emit()
-	$BeatSyncEffect.scale = Vector2(10,10)
-	$BeatSyncEffect.modulate = Color.CADET_BLUE
-	$BeatSyncEffect.restart()
+	vfx.scale = Vector2(10,10)
+	vfx.modulate = Color.CADET_BLUE
+	vfx.restart()
