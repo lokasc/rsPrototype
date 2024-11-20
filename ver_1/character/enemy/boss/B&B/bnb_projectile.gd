465ab9dd84ccd686fed3f58780f234ab80ff9bd0
lokasc <lucaschan2012his@gmail.com>
2024-11-21 04:56:18 +0800

    Removed testing print statements

--- ver_1/character/enemy/boss/B&B/bnb_projectile.gd
+++ ver_1/character/enemy/boss/B&B/bnb_projectile.gd
@@ -39,8 +39,6 @@ func _ready() -> void:
 		circle_container.visible = false
 		if tile_scale != Vector2.ZERO:
 			projectile_container.scale = tile_scale
-	
-	print("Projectile",dmg)
 
 func _process(delta: float) -> void:
 	if is_rain:
